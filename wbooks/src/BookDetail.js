47d14478a742675c4f6de0fefff2192df06ce4b2
Martin <martinlabate@gmail.com>
2018-02-02 18:03:52 -0300

    Added book detail

--- wbooks/src/BookDetail.js
+++ wbooks/src/BookDetail.js
@@ -1,31 +1,60 @@
 import React, { Component } from 'react';
-import defaultBookIcon from './default_book.svg'
+import myBooks from './books.json';
+import { Link } from 'react-router-dom';
+import defaultBookIcon from './default_book.svg';
 import './BookDetail.css';
 
 class BookDetail extends Component {
-  constructor() {
-    super()
-  }
-
   render() {
+    var book = myBooks.find(book => {
+      return book.id == this.props.match.params.id;
+    })
     return (
       <div className="body">
+        <Link className="back" to={'/dashboard'}>&lt; Volver</Link>
         <div className="book-detail">
-          <img className="book-cover" />
+        {(book.image_url === null) ? (
+          <img className="default-book-cover" src={defaultBookIcon} alt="Book cover"/>
+        ) : (
+          <img className="book-cover" src={book.image_url} alt="Book cover"/>
+        )}
           <div className="book-info">
-            <b className="title" />
-            <p className="detail" />
-            <p className="detail" />
-            <p className="detail" />
-            <p className="description" />
+            <b className="title">{book.title} </b>
+            <p className="author">{book.author} </p>
+            <p className="year">{book.year} </p>
+            <p className="genre">{book.genre} </p>
+            <p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <button className="rent-button">Alquilar</button>
           </div>
         </div>
-        <div className="suggestions">
+        <div className="separation" />
+        <div className="sugestions">
+          <b className="section-title">Sugerencias</b>
+          <div className="sugestions-list">
+            <img className="sugestions-placeholder" src={defaultBookIcon} alt="Book cover"/>
+            <img className="sugestions-placeholder" src={defaultBookIcon} alt="Book cover"/>
+            <img className="sugestions-placeholder" src={defaultBookIcon} alt="Book cover"/>
+            <img className="sugestions-placeholder" src={defaultBookIcon} alt="Book cover"/>
+          </div>
         </div>
+        <div className="separation" />
         <div className="comments">
+          <b className="section-title">Comentarios</b>
+          <div className="new-comment">
+            <img className="profile-icon" src={defaultBookIcon} />
+            <div className="write-comment">
+              <b className="comment">Agregar comentario</b>
+              <form className="comment-container" onSubmit={this.handleSubmit}>
+                <input className="comment-bar" type="text" onChange={this.handleChange} />
+                <button className="comment-button" type="submit" value="Submit"
+                onClick={this.handleSubmit}>Enviar</button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     )
   }
 }
+
+export default BookDetail;
