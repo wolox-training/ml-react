fe83b36848eecffadd62998e09a5336937b1915e
Martin <martinlabate@gmail.com>
2018-02-06 14:15:38 -0300

    Added book detail (#4)
    * Added book detail

--- wbooks/src/App.js
+++ wbooks/src/App.js
@@ -1,40 +1,13 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from './wbooks_logo.svg';
 import notifications from './notifications.svg';
 import addBook from './add_book.svg';
-import search from './search.svg';
-import bookIcon from './default_book.svg'
 import './App.css';
-import Books from './Books'
+import Dashboard from './Dashboard'
+import BookDetail from './BookDetail'
 
 class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      filterType: '',
-      searchText: '',
-      searchTextChange: '',
-      filterTypeChange: 'author'
-    };
-  }
-
-  handleFilter = (event) => {
-    console.log("It's filtering");
-    this.setState({filterTypeChange: event.target.value});
-  }
-
-  handleChange = (event) => {
-    this.setState({searchTextChange: event.target.value})
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault()
-    this.setState({
-      filterType: this.state.filterTypeChange ,
-      searchText: this.state.searchTextChange
-    })
-  }
 
   render() {
     return (
@@ -42,25 +15,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <div className="App-menu">
-            <img src={notifications} className="App-menu-item" />
-            <img src={addBook} className="App-menu-item" />
-            <img src={addBook} className="App-profile-img" />
+            <img src={notifications} className="App-menu-item" alt="notif icon"/>
+            <img src={addBook} className="App-menu-item" alt="add book icon"/>
+            <img src={addBook} className="App-profile-img" alt="profile img"/>
           </div>
         </header>
-        <div className="Nav">
-          <select className="filter-bar" placeholder="Seleccionar Filtro"
-          value={this.state.filterTypeChange} onChange={this.handleFilter}>
-            <option value="author">Autor</option>
-            <option value="title">Titulo</option>
-          </select>
-          <form className="search-container" onSubmit={this.handleSubmit}>
-            <input className="search-bar" type="text" placeholder="Buscar..."
-            onChange={this.handleChange} />
-            <img className="search-icon" type="submit" src={search} value="Submit"
-            onClick={this.handleSubmit}/>
-          </form>
-        </div>
-        <Books filterType={this.state.filterType} filterText={this.state.searchText}/>
+        <Switch>
+          <Route exact path='/dashboard' component={Dashboard}/>
+          <Route path='/books/:id' component={BookDetail}/>
+        </Switch>
       </div>
     );
   }
--- wbooks/src/Books.js
+++ wbooks/src/Books.js
@@ -1,37 +1,32 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import myBooks from './books.json';
 import defaultBookIcon from './default_book.svg'
 import './Books.css';
+import BookCover from './BookCover'
 
 class Books extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   filteredBooks = () => {
-    if (this.props.filterText == '' || this.props.filterType == '') {
+    if (this.props.filterText === '' || this.props.filterType === '') {
       return myBooks;
     }
-
     return myBooks.filter(book => {
-      console.log((this.props.filterText));
       return book[this.props.filterType].toUpperCase().includes(this.props.filterText.toUpperCase());
     });
   }
 
   render(){
      return (
-       <div className="body">
+       <div className="grid">
         {this.filteredBooks().map(item =>
-          <div className="book">
-          {(item.image_url == null) ? (
-            <img className="default-book-icon" src={defaultBookIcon} />
-          ) : (
-            <img className="book-icon" src={item.image_url} />
-          )}
-            <b className="book-title" >{item.title}</b>
-            <p className="book-author" >{item.author}</p>
-          </div>)}
+          <Link to={`/books/${item.id}`} key={item.id}>
+            <div className="book">
+              <BookCover imageUrl={item.image_url} defaultCover="default-book-icon" cover="book-icon"/>
+              <b className="book-title" >{item.title}</b>
+              <p className="book-author" >{item.author}</p>
+            </div>
+          </Link >)}
        </div>
      );
    }
