acec924bb18929b41dc449c098b9c60c774aef81
Martin <martinlabate@gmail.com>
2018-02-23 17:19:44 -0300

    changed to redux form (#8)

--- wbooks/src/redux/Filter/actions.js
+++ wbooks/src/redux/Filter/actions.js
@@ -1,15 +1,5 @@
-export const CHANGE_FILTER = 'FILTER@@CHANGE_FILTER';
 export const APPLY_FILTER = 'FILTER@@APPLY_FILTER';
 
-export const filters = {
-  TITLE: 'title',
-  AUTHOR: 'author'
-};
-
-export function changeFilter(payload) {
-  return {type: CHANGE_FILTER, payload};
-};
-
-export function applyFilter() {
-  return {type: APPLY_FILTER};
+export function applyFilter(payload) {
+  return {type: APPLY_FILTER, payload};
 }
--- wbooks/src/redux/Text/reducer.js
+++ wbooks/src/redux/Text/reducer.js
@@ -1,21 +1,15 @@
-import {CHANGE_TEXT, APPLY_TEXT} from './actions'
+import {APPLY_TEXT} from './actions'
 
 const initialState = {
   searchText: '',
-  searchTextChange: ''
 };
 
 export default function text(state = initialState, action) {
   switch (action.type) {
-    case CHANGE_TEXT:
-      return {
-        ...state,
-        searchTextChange: action.payload
-      };
     case APPLY_TEXT:
       return {
         ...state,
-        searchText: state.searchTextChange
+        searchText: action.payload
       };
     default:
       return state;
